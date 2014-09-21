872c92a4dfb373458a020fc50dc64a79ee103615
torque <torque@1>
2014-09-20 23:41:07 -0700

    Keyselect: remove extern "C" from header.
    This was the only header with this, and the code should be valid c++,
    so there's no reason to keep it.

--- Source/keyselect.h
+++ Source/keyselect.h
@@ -1,16 +1,12 @@
 // keyselect.h
 #pragma once
 
-#ifdef __cplusplus
-extern "C"
-#endif
 int SDLTypingFilter(const SDL_Event *event);
 
 void StartWatchingTyping();
 void StopWatchingTyping();
 bool CheckTyping( char* ascii, SDLKey* sdl );
 void CheckKeys();
-
 
 extern SDLKey playerKeys[2][4];
 extern const SDLKey defaultPlayerKeys[2][4];
